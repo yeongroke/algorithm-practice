2dbe5034eb6030bcf80274448e2236e6e9178d6f
YEONG ROK KIM <chfaos153@naver.com>
2022-09-18 22:13:22 +0900

    Update D20220918_1.java

--- september/D20220918_1.java
+++ september/D20220918_1.java
@@ -3,4 +3,24 @@ package daliy_coding.september;
 import java.util.Stack;
 
 public class D20220918_1 {
+
+    // solution method
+    static int solution(String s) {
+        if(s.length() <= 1)
+            return 0;
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : s.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == c) stack.pop();
+            else stack.push(c);
+        }
+
+        return stack.isEmpty() ? 1 : 0;
+    }
+
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution("baabaa"));
+    }
 }
