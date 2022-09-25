9979a4a36f6d5e21055f0628760b0497e57cb242
YEONG ROK KIM <chfaos153@naver.com>
2022-09-25 22:01:47 +0900

    Create D20220925_1.java

---
+++ september/D20220925_1.java
@@ -0,0 +1,13 @@
+package daliy_coding.september;
+
+import java.util.Arrays;
+
+/*
+* 프로그래머스 구명보트
+* */
+public class D20220925_1 {
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution(people, limit));
+    }
+}
