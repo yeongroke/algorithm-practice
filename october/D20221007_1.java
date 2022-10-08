95f9688672b63b72327086b0fc57cf2a1fdc50e0
YEONG ROK KIM <chfaos153@naver.com>
2022-10-08 16:33:44 +0900

    Update D20221007_1.java

--- october/D20221007_1.java
+++ october/D20221007_1.java
@@ -6,4 +6,29 @@ import java.util.*;
 * 프로그래머스 올바른 괄호
 * */
 public class D20221007_1 {
+
+    static String s = "(())())";
+
+    // solution method
+    static boolean solution(String s) {
+        boolean answer = true;
+
+        if(s.charAt(0) == ')')
+            return false;
+
+        Stack<Character> stack = new Stack<>();
+        for(int i=0; i<s.length(); i++) {
+            if(!stack.isEmpty() && s.charAt(i) == ')')
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+
+        return stack.isEmpty();
+    }
+
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution(s));
+    }
 }
