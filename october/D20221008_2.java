0d1437ba942a38141a94ccc702df63e770168ccd
YEONG ROK KIM <chfaos153@naver.com>
2022-10-09 12:02:59 +0900

    Update D20221008_2.java

--- october/D20221008_2.java
+++ october/D20221008_2.java
@@ -8,6 +8,30 @@ import java.util.Queue;
 * */
 public class D20221008_2 {
 
+    static int cacheSize = 3;
+    static String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
+
+    // solution method
+    static int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+
+        Queue<String> queue = new LinkedList<>();
+
+        for(int i=0; i<cities.length; i++) {
+            if(queue.contains(cities[i].toLowerCase())) {
+                answer ++;
+                queue.remove(cities[i].toLowerCase());
+            } else {
+                answer += 5;
+            }
+            queue.offer(cities[i].toLowerCase());
+            if(queue.size() > cacheSize)
+                queue.poll();
+        }
+
+        return answer;
+    }
+
     // coding test run
     public static void main(String[] args) {
         System.out.println(solution(cacheSize, cities));
