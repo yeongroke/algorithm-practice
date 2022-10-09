ae853d53937a5b06e70f51b7cd31994b773b755f
YEONG ROK KIM <chfaos153@naver.com>
2022-10-09 12:02:49 +0900

    Create D20221008_2.java

---
+++ october/D20221008_2.java
@@ -0,0 +1,15 @@
+package daliy_2022coding.october;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+* 프로그래머스 캐시
+* */
+public class D20221008_2 {
+
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution(cacheSize, cities));
+    }
+}
