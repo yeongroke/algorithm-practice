5e3ba2ed3342ae2ee7a39c996dbb338db2592106
YEONG ROK KIM <chfaos153@naver.com>
2022-10-09 17:28:29 +0900

    Update D20221009_1.java

--- october/D20221009_1.java
+++ october/D20221009_1.java
@@ -7,4 +7,25 @@ import java.util.Scanner;
 *
 * */
 public class D20221009_1 {
+    // solution method
+    static int solution(int n) {
+        int answer = 0;
+        int copy = n;
+
+        while(true) {
+            n = ((n % 10) * 10) + (((n / 10) + (n % 10)) % 10);
+            answer ++;
+            if(n == copy)
+                break;
+        }
+
+        return answer;
+    }
+
+    // coding test run
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        System.out.println(solution(n));
+    }
 }
