9b0875f8aed96ec003fc4f9e2d96216261ca7683
YEONG ROK KIM <chfaos153@naver.com>
2022-10-30 14:18:36 +0900

    Update D20221030_1.java

--- october/D20221030_1.java
+++ october/D20221030_1.java
@@ -3,4 +3,29 @@
 * 45. Jump Game II
 * */
 public class D20221030_1 {
+    // solution method
+    static int solution(int[] nums) {
+        if(nums.length == 1)
+            return 0;
+        int answer = 0;
+
+        int nowIndex = 0, nextIndex = 0;
+        for(int i=0; i<nums.length-1; i++) {
+            if(nextIndex < nums[i] + i)
+                nextIndex = nums[i] + i;
+            if(i == nowIndex) {
+                nowIndex = nextIndex;
+                answer ++;
+            }
+        }
+
+        return answer;
+    }
+
+    static int[] nums = {2,3,1,1,4};
+
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution(nums));
+    }
 }
