9d2a788c2b8e4eef76a68db6673782f7cd4b1faf
YEONG ROK KIM <chfaos153@naver.com>
2022-10-30 14:17:34 +0900

    Update D20221029_1.java

--- october/D20221029_1.java
+++ october/D20221029_1.java
@@ -7,4 +7,52 @@ import java.util.Stack;
 * 145. Binary Tree Postorder Traversal
 * */
 public class D20221029_1 {
+
+    static class TreeNode {
+         int val;
+         TreeNode left;
+         TreeNode right;
+        TreeNode() {
+
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // solution method
+    static List<Integer> solution(TreeNode root) {
+        List<Integer> answer = new ArrayList<>();
+
+        if(root == null)
+            return answer;
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while(!stack.empty()) {
+            TreeNode subRoot = stack.pop();
+            answer.add(0,subRoot.val);
+            if(subRoot.left != null)
+                stack.push(subRoot.left);
+            if(subRoot.right != null)
+                stack.push(subRoot.right);
+        }
+
+        return answer;
+    }
+
+    static TreeNode treeNode = new TreeNode(1);
+
+    // coding test run
+    public static void main(String[] args) {
+        System.out.println(solution(treeNode));
+    }
 }
